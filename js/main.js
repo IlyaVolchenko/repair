3f6269d098c8405d08d0ebf9d6fd127bec14f465
IlyaVolchenko <volchenko.ilya7@gmail.com>
2021-11-11 11:49:38 +0800

    repair

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ $(function(){
 $('.slider__innet').slick({
    dots: true
 });
+});
 
+let modal = document.querySelector('.modal');
+let modalBtn = document.querySelector('.btn');
+let modalBg = document.querySelector('.modal__bg');
 
+modalBtn.addEventListener('click', function(){
+  modal.classList.add('show');
+  modalBg.classList.add('show');
+});
+
+document.addEventListener('click', function(e){
+  let click = e.target.classList.value;
+  if (click === 'modal__bg show') {
+    modal.classList.remove('show');
+    modalBg.classList.remove('show');
+  }
 });
